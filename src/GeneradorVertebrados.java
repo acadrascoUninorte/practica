49c0186fb3cc479a9eb5ff96c6637dd1b6d900d9
acadrascoUninorte <acadrasco@uninorte.edu.co>
2022-09-08 20:09:40 -0500

    Anexo de un nuevo vertebrado

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,10 +1,11 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados(){
-        Vertebrado[] miVertebrados = new Vertebrado[5];
+        Vertebrado[] miVertebrados = new Vertebrado[6];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("Perro", "Mamifero"); 
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
+        miVertebrados[3] = new Vertebrado("Tiburón", "Pez"); 
+        miVertebrados[4] = new Vertebrado("Persona", "Mamifero");
         
         return miVertebrados;
     }
